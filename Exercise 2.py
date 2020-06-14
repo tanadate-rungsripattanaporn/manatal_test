770b9c077e4177d139ad53945d37c8352655f0a6
tanadate-rungsripattanaporn <66917530+tanadate-rungsripattanaporn@users.noreply.github.com>
2020-06-15 02:38:33 +0700

    Update Exercise 2.py

--- Exercise 2.py
+++ Exercise 2.py
@@ -6,5 +6,6 @@ for _ in range(10):
     box.remove(select)
     pick.append(select)
 pick.sort()
+#use len to see that still 50 balls or can count total score of ball on pick,box==50balls
 print('all picked balls is',pick,'total=',len(pick))
 print('all left balls in box',box,'total=',len(box))
